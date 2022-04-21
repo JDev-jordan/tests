c3fdbf0eef5f5e40e73ef965313ba7f6c9458544
JDev-jordan <75861498+JDev-jordan@users.noreply.github.com>
2022-04-21 08:05:13 +0200

    initialisation projet

---
+++ testPositiveThinkingCompany/src/main/java/com/jdev/testPositiveThinkingCompany/TestPositiveThinkingCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.testPositiveThinkingCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPositiveThinkingCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPositiveThinkingCompanyApplication.class, args);
+	}
+
+}
---
+++ testPositiveThinkingCompany/src/test/java/com/jdev/testPositiveThinkingCompany/TestPositiveThinkingCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.testPositiveThinkingCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPositiveThinkingCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
